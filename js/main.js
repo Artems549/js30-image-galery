8e937c6e26e50a57dda31ac2afafa629d5956bcc
Artem <sfmillingg@gmail.com>
2023-09-29 23:57:54 +0400

    feat: add styles in main

--- js/main.js
+++ js/main.js
@@ -2,8 +2,30 @@ const form = document.querySelector('.header__search form');
 const formValue = document.getElementById('value');
 const formSubmit = document.getElementById('submit');
 const formCleanBtn = document.querySelector('.header__search-close');
-const itemPic = document.querySelectorAll('.main__items-item');
+const itemsPic = document.querySelectorAll('.main__items-item');
+const modal = document.querySelector('.modal');
+const modalCloseBtn = document.querySelector('.modal__close')
+const overlay = document.querySelector('.overlay');
 
+
+
+
+function openModal() {
+    modal.style.display = 'flex';
+    modal.style.animation = 'on-modal 1s forwards';
+    overlay.style.display = 'block';
+    overlay.style.animation = 'on-modal 1s forwards'
+}
+
+function closeModal() {
+    modal.style.animation = 'off-modal 1s forwards';
+    overlay.style.animation = 'off-modal 1s forwards'
+}
+for(let i = 0; i < itemsPic.length; i++) {
+    itemsPic[i].addEventListener('click', openModal)
+}
+overlay.addEventListener('click', closeModal)
+modalCloseBtn.addEventListener('click', closeModal)
 
 
 
