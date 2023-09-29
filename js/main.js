fc14895bb65523b085dc911f7748ee3a269dcda9
Artem <sfmillingg@gmail.com>
2023-09-29 22:40:59 +0400

    feat: add header

--- js/main.js
+++ js/main.js
@@ -1,2 +1,26 @@
-const itemPic = document.querySelectorAll('.main__items-item')
-console.log(itemPic.length)
+const form = document.querySelector('.header__search form');
+const formValue = document.getElementById('value');
+const formSubmit = document.getElementById('submit');
+const formCleanBtn = document.querySelector('.header__search-close');
+const itemPic = document.querySelectorAll('.main__items-item');
+
+
+
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+})
+function cleanForm() {
+    formCleanBtn.addEventListener('click', function() {
+        formValue.value = ''
+    })
+}
+cleanForm()
+
+function sendValue() {
+    formSubmit.addEventListener('click', function() {
+        const value = formValue.value;
+        console.log(value)
+    })
+}
+sendValue()
